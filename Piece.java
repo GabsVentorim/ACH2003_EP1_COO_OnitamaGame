b4cb69b085cccc22f0dd06957c9f2f3ced5cf740
Gabriel Ventorim <gabriel.ventorims@gmail.com>
2023-06-01 21:56:47 -0300

    Completing the class Piece

--- Piece.java
+++ Piece.java
@@ -2,36 +2,51 @@
  * Classe que contém informações das peças de jogo
  */
 public class Piece {
+
+    private final Color color;
+    private final boolean isMaster;
+    private boolean isAlive;
+
     /**
      * Construtor que define a cor e o tipo da peça
-     * @param color Cor da peça
+     * 
+     * @param color    Cor da peça
      * @param isMaster Se o tipo da peça é mestre ou não
      */
     public Piece(Color color, boolean isMaster) {
-
+        this.color = color;
+        this.isMaster = isMaster;
+        isAlive = true;
     }
 
     /**
      * Método que devolve a cor da peça
+     * 
      * @return Enum Color com a cor da peça
      */
     public Color getColor() {
-        return null;
+        return color;
     }
 
     /**
      * Método que devolve se é um mestre ou não
+     * 
      * @return Booleano true para caso seja um mestre e false caso contrário
      */
     public boolean isMaster() {
-        return false;
+        return isMaster;
     }
 
     /**
      * Método que devolve se a peça ainda está em jogo ou não
+     * 
      * @return Booleano true para caso esteja em jogo e false caso contrário
      */
     public boolean isAlive() {
-        return false;
+        return isAlive;
+    }
+
+    public void isDead() {
+        isAlive = false;
     }
 }
